e747aa9cd6b280a2fb8227766e0d3b0b93ee054f
BTFitti <61165901+BTFitti@users.noreply.github.com>
2022-03-22 19:04:08 -0300

    Adicionando projeto Calculadora de Média

---
+++ calculadora-de-m-dia-completoid3/src/script.js
@@ -0,0 +1,13 @@
+var nome = "Millena"
+var notaBim1 = 10
+var notaBim2 = 10
+var notaBim3 = 10
+var notaBim4 = 10
+
+var notaFinal = (notaBim1 + notaBim2 + notaBim3 + notaBim4) / 4
+
+var notaFixada = notaFinal.toFixed(1)
+
+
+console.log("Bem vinda " + nome)
+console.log("Sua nota é incrível: " + notaFixada) 
